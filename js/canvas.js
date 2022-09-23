7300c616d0230f0e0c4b3598d1a11938c24deae3
StephenLasky <StephenJLasky@gmail.com>
2022-09-23 11:25:39 -0500

    Adding working canvas example

---
+++ js/canvas.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+const height = 1080;
+const width = 1920;
+
+// resize canvas (CSS does scale it up or down)
+canvas.height = height;
+canvas.width = width;
+
+context.fillRect(100, 150, 200, 300);
+
+// create a stroke
+context.beginPath();
+context.moveTo( 50, 100);
+context.lineTo(100, 150);
+context.stroke();
+// this ends the current path
+context.beginPath();
+
+context.strokeStyle = 'red';
+context.fillStyle = 'red';
+
+let drawing = false;
+
+function startDrawing(e) {
+    drawing = true;
+    context.beginPath();
+}
+
+window.addEventListener("mousedown", startDrawing);
+
+function endDrawing(e) {
+    drawing = false;
+}
+
+window.addEventListener("mouseup", endDrawing);
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect(),
+      scaleX = canvas.width / rect.width,
+      scaleY = canvas.height / rect.height;
+
+    return {
+      x: (evt.clientX - rect.left) * scaleX,
+      y: (evt.clientY - rect.top) * scaleY
+    }
+}
+
+function draw(e) {
+    if (!drawing) return;
+
+    let { x, y } = getMousePos(canvas, e);
+
+    context.lineTo(x, y);
+    context.stroke();
+}
+
+window.addEventListener("mousemove", draw);
